a9e6d6eb1c48de4c60232a123b77f2d13c86278b
dmi3coder <dmi3coder@gmail.com>
2016-05-29 14:06:25 +0300

    Blebby class visivility

--- core/src/com/dmi3coder/sprites/Blebby.java
+++ core/src/com/dmi3coder/sprites/Blebby.java
@@ -9,10 +9,21 @@ import com.badlogic.gdx.graphics.g2d.Sprite;
  * Created by dmi3coder on 28/05/16;22:15.
  */
 public class Blebby extends Sprite {
+    private boolean visible = true;
+    private float capacity = 0.01f;
 
     public Blebby(Texture texture){
         super(texture);
         setColor(new Color((int)(Math.random() * 0x1000000)));
     }
+
+    public void setVisible(boolean b){
+        visible = b;
+    }
+
+    public boolean isVisibile(){
+        return visible;
+    }
+
 
 }
