0602d38962d2cae34ebed814db5bbadfce735b2f
dmi3coder <dmi3coder@gmail.com>
2016-05-31 19:15:08 +0300

    unworking LoginScreen

---
+++ core/src/com/dmi3coder/blebbio/LoginScreen.java
@@ -0,0 +1,80 @@
+package com.dmi3coder.blebbio;
+
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Screen;
+import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.g2d.BitmapFont;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.graphics.g2d.TextureAtlas;
+import com.badlogic.gdx.scenes.scene2d.Stage;
+import com.badlogic.gdx.scenes.scene2d.ui.*;
+import com.badlogic.gdx.utils.Align;
+import com.badlogic.gdx.utils.viewport.ScalingViewport;
+import com.badlogic.gdx.utils.viewport.ScreenViewport;
+
+import javax.swing.*;
+
+/**
+ * Created by dmi3coder on 31/05/16;15:20.
+ */
+public class LoginScreen implements Screen {
+    private final Treegrassio game;
+    private SpriteBatch batch;
+    private Stage stage;
+
+
+    public LoginScreen(Treegrassio game, SpriteBatch batch){
+        this.game = game;
+        this.batch = batch;
+        stage = new Stage(new ScreenViewport());
+        Gdx.input.setInputProcessor(stage);
+        Skin skin = new Skin(Gdx.files.internal("uiskin.json"));
+        TextField button = new TextField("Click me", skin,"default");
+        button.setWidth(200f);
+        button.setHeight(20f);
+        button.setPosition(Gdx.graphics.getWidth() /2 - 100f, Gdx.graphics.getHeight()/2 - 10f);
+        stage.addActor(button);
+        Gdx.input.setInputProcessor(stage);
+    }
+
+    @Override
+    public void render(float delta) {
+        Gdx.gl.glClearColor(0.2f, 0.2f, 0.2f, 1);
+        Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+        stage.act(Math.min(Gdx.graphics.getDeltaTime(), 1 / 30f));
+        batch.begin();
+        stage.draw();
+        batch.end();
+    }
+
+    @Override
+    public void resize(int width, int height) {
+
+    }
+
+    @Override
+    public void pause() {
+
+    }
+
+    @Override
+    public void resume() {
+
+    }
+
+    @Override
+    public void hide() {
+
+    }
+
+    @Override
+    public void dispose() {
+
+    }
+
+    @Override
+    public void show() {
+
+    }
+
+}
